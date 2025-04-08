30a0f64c75dcfabc7844e36836d1b509ecab77c2
Tian Zilu <zilu.tian@uzh.ch>
2025-04-08 15:04:00 +0200

    Add generator script for fully connected neural nets

--- tests/graphGen.py
+++ tests/graphGen.py
@@ -1,8 +1,7 @@
 import sys
 import os
+import json
 
-# Graph for Example 6.1
-# Graph for Example 6.2
 save_dir = "resources/"
 
 def gen_edges(vertex, labels):
@@ -30,6 +29,36 @@ def gen_path_bsc(graph_size, file_name):
             vid += 1
         file.write(f"{vid} c {vid+1}")
 
+# fully connected neural network
+# nodes in the first layer are associated with a self-loop labelled with a
+# nodes in the last layer are associated with a self-loop labelled with c
+# edges connecting layers are labelled with b
+def gen_cnn(layers, file_name):
+    S = layers[0]
+    T = layers[-1]
+    total_nodes = sum(layers)
+
+    print(f"Write to {file_name} a {len(layers)}-layer graph with {S} source nodes, {T} target nodes, and {total_nodes} nodes")
+
+    with open(file_name, "w") as f:
+        prefix_sum = [0]
+        for p in layers:
+            prefix_sum.append(prefix_sum[-1] + p)
+
+        # Add a self-loop with label a on the source nodes
+        for i in range(S):
+            f.write(f"{i} a {i}\n")
+
+        # Add a self-loop with label b on the target nodes
+        for i in range(total_nodes - T, total_nodes):
+            f.write(f"{i} c {i}\n")
+
+        # Add edges with label b between intermediate nodes
+        for i in range(1, len(prefix_sum)-1):
+            for j in range(prefix_sum[i-1], prefix_sum[i]):
+                for k in range(prefix_sum[i], prefix_sum[i+1]):
+                    f.write(f"{j} b {k}\n")
+
 def gen_disjoint_cycles(cycle_size, num_cycles, file_name):
     labels = ["b", "c"]
 
@@ -45,27 +74,22 @@ def gen_disjoint_cycles(cycle_size, num_cycles, file_name):
 if (__name__ == "__main__"):
         # Get user input
         if len(sys.argv) > 1:
-            graph_type = int(sys.argv[1])
-            graph_size = int(sys.argv[2])
-            if (graph_type == 1):
-                graph_file_name = save_dir+f"path_{graph_size}.txt"
-            elif (graph_type == 2):
-                graph_file_name = save_dir+f"disjoint_cycles_{graph_size}.txt"
-            elif (graph_type == 3):
-                graph_file_name = save_dir+f"pathbsc_{graph_size}.txt"
+            config_file = sys.argv[1]
 
+            f = open(f"conf/{config_file}.json")
+            config = json.load(f)
+            graph_file_name = config["output_graph"]
+            query = config["query"]
             print(f"The graph will be saved as {graph_file_name}")            
-            
+
             if os.path.exists(graph_file_name):
                 print(f"Graph file {graph_file_name} exists!")
                 exit()
-            else:
-                if (graph_type == 1):
-                    gen_path_bs(graph_size, graph_file_name)
-                elif (graph_type == 2):
-                    gen_disjoint_cycles(graph_size, 2, graph_file_name)
-                elif (graph_type == 3):
-                    gen_path_bsc(graph_size, graph_file_name)
+            
+            graph_generator = config["generator"]
+            if graph_generator in globals() and callable(globals()[graph_generator]):
+                func = globals()[graph_generator]
+                result = func(*[config[arg] for arg in config["gen_args"]])
 
         else:
             print("No input provided. Please pass the graph type, size, and file name as command-line arguments.")
