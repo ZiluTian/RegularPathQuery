b20dae73bf0e95c0928f31c57a50afe0c2e1bcab
Tian Zilu <zilu.tian@uzh.ch>
2025-05-02 12:31:54 +0200

    Add sparse nn

--- tests/graphGen.py
+++ tests/graphGen.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+import random
 
 save_dir = "resources/"
 
@@ -58,6 +59,49 @@ def gen_nn(layers, file_name):
             for j in range(prefix_sum[i-1], prefix_sum[i]):
                 for k in range(prefix_sum[i], prefix_sum[i+1]):
                     f.write(f"{j} b {k}\n")
+
+# heterogeneous nn with specified connecitivity.
+# connectivity[0]: the percentage of nodes in the first layer that are starting nodes. Otherwise, a node is added with self-loop b 
+# connectivity[-1]: the percentage of nodes in the layer layer that are accepting nodes. Otherwise, a node is added with self-loop b
+# connectivity[i] (intermediate layers): the likelihood that a node in the previous layer is connected to all the nodes in the next layer. 
+#   Otherwise, a node in the previous layer has light_edge_connectivity likelihood (per edge) of connecting with a node in the next layer
+def gen_nn_sparse(layers, connectivity, light_edge_connectivity, file_name):
+    S = layers[0]
+    T = layers[-1]
+    total_nodes = sum(layers)
+    assert(len(connectivity) == len(layers) + 1)
+
+    print(f"Write to {file_name} a {len(layers)}-layer graph with {S} source nodes, {T} target nodes, and {total_nodes} nodes")
+
+    with open(file_name, "w") as f:
+        prefix_sum = [0]
+        for p in layers:
+            prefix_sum.append(prefix_sum[-1] + p)
+
+        # Add a self-loop with label a on the source nodes
+        for i in range(S):
+            if (random.random() < connectivity[0]):
+                f.write(f"{i} a {i}\n")
+            else:
+                f.write(f"{i} b {i}\n")
+
+        # Add a self-loop with label c on the target nodes
+        for i in range(total_nodes - T, total_nodes):
+            if (random.random() < connectivity[-1]):
+                f.write(f"{i} c {i}\n")
+            else:
+                f.write(f"{i} b {i}\n")
+            
+        # Add edges with label b between intermediate nodes according to the connectivity
+        for i in range(1, len(prefix_sum)-1):
+            for j in range(prefix_sum[i-1], prefix_sum[i]):
+                if (random.random() < connectivity[i]):
+                    for k in range(prefix_sum[i], prefix_sum[i+1]):
+                        f.write(f"{j} b {k}\n")
+                else:
+                    for k in range(prefix_sum[i], prefix_sum[i+1]):
+                        if (random.random() < light_edge_connectivity):
+                            f.write(f"{j} b {k}\n")
 
 # only two nodes in the last layer are considered as accepting
 def gen_nn_binary_classifier(layers, file_name):
